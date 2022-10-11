6f3bb274109e949958756bd11f553a454d3fa878
Raymond Loman <r.loman@recognize.nl>
2022-10-11 13:23:41 +0200

    Add CarService

---
+++ src/app/services/car.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import { Car } from '../model/car'
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+
+  url = 'http://localhost:3000/api/cars';
+
+  constructor(private http: HttpClient) { }
+
+  public findAll(): Observable<Car[]> {
+
+    return this.http.get<Car[]>(this.url);
+  }
+}
